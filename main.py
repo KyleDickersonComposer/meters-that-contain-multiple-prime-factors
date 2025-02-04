814aa9f6d233282c7050b67638ab8cf9fb1c061f
Kyle Dickerson <kyledickersoncomposer@gmail.com>
2025-02-04 18:02:55 -0500

    update stuff

--- main.py
+++ main.py
@@ -46,10 +46,15 @@ prime_numbers = [
     97,
 ]
 
-for i in range(1, 100):
+for i in range(2, 100):
     meter = prime_factors_of_N(i)
-    if len(meter) > 1:
-        message = f"{i} has factors "
-        for j in range(len(meter)):
-            message += str(f"({int(meter[j])})")
-        print(message)
+    if len(meter) == 1 or len(meter) == 3:
+        continue
+
+    if meter[0] > 20 and (meter[1] == 2 or meter[1] == 3):
+        continue
+
+    message = f"{i} has prime factors "
+    for j in range(len(meter)):
+        message += str(f"({int(meter[j])})")
+    print(message)
